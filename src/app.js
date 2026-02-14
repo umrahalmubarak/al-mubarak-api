bffa1ae95a75072f973aab525283c35ecd591da2
prajwaldots <prajwal.s2294@gmail.com>
2026-02-14 19:17:58 +0530

    rate limit changed to 500

--- src/app.js
+++ src/app.js
@@ -31,7 +31,7 @@ app.use(
 // Rate limiting
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // limit each IP to 100 requests per windowMs
+  max: 500, // limit each IP to 100 requests per windowMs
   message: {
     success: false,
     message: "Too many requests from this IP, please try again later.",
