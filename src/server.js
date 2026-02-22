061596879eb5f4e22e9d6027597b20056f98c828
prajwaldots <prajwal.s2294@gmail.com>
2026-02-22 20:52:25 +0530

    server.js changed

--- src/server.js
+++ src/server.js
@@ -1,15 +1,10 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import app from "./app.js";
-import { PORT } from "./config/env.js";
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
-// import dotenv from "dotenv";
-// dotenv.config();
-
-// import app from "./app.js";
-// import { PORT } from "./config/env.js";
-
-// app.listen(PORT, "0.0.0.0", () => {
-//   console.log(` Server running on port ${PORT}`);
-// });
